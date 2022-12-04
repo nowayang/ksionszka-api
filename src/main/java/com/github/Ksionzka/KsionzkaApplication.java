62be3d2d59af8fe1b6b8838e4106288fd3ffc9e3
kamil.nowak <kamil.nowak@faust-it.com>
2022-12-04 19:39:06 +0100

    Initial commit

---
+++ src/main/java/com/github/Ksionzka/KsionzkaApplication.java
@@ -0,0 +1,13 @@
+package com.github.Ksionzka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsionzkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsionzkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Ksionzka/KsionzkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Ksionzka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsionzkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
