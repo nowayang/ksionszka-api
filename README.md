2b1da159453819620557aa79d604bd5383037df8
Wojciech Najdek <najdekwojtek1@gmail.com>
2023-01-12 00:25:46 +0100

    Add sendRecentReservations

--- README.md
+++ README.md
@@ -39,3 +39,25 @@ MailDev (do testowania generowanych maili)
 docker run -p 1025:1025 -p 1080:1080 --name maildev maildev/maildev
 ```
 
+#### Testowanie websocketu (informacja o ostatio zarezerwowanych książkach):
+tworzymy taki plik i odpalamy w przeglądarce
+```html
+<html>
+<head>
+    <script src="https://cdn.jsdelivr.net/npm/sockjs-client@1/dist/sockjs.min.js"></script>
+    <!-- download stomp from https://raw.githubusercontent.com/stomp-js/stomp-websocket/master/lib/stomp.js -->
+    <script src="./stomp.js"></script>
+    <script type="text/javascript">
+        var url = "ws://localhost:8080/ws";
+        var client = Stomp.client(url);
+
+        client.connect({}, function(frame) {
+            client.subscribe("/topic/recent-reservations", function(message) {
+                document.body.innerHTML = message;
+            });
+        })
+    </script>
+</head>
+</html>
+```
+
--- src/main/java/com/github/Ksionzka/security/WebSecurityConfig.java
+++ src/main/java/com/github/Ksionzka/security/WebSecurityConfig.java
@@ -5,12 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,26 +14,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-//	@Bean
-//	public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-//		UserDetails user = User.withUsername("user")
-//				.password(passwordEncoder.encode("password"))
-//				.roles("USER")
-//				.build();
-//
-//		UserDetails admin = User.withUsername("admin")
-//				.password(passwordEncoder.encode("admin"))
-//				.roles("USER", "ADMIN")
-//				.build();
-//
-//		return new InMemoryUserDetailsManager(user, admin);
-//	}
-
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.antMatchers("/api/register/**").permitAll()
-//				.antMatchers("/h2/**").hasRole("ADMIN")
 				.antMatchers("/h2/**").permitAll()
 				.anyRequest().authenticated()
 				.and()
