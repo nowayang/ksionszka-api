67b6d443ba5621780fd0a6ecfd9367d2f8f73c3e
Wojciech Najdek <najdekwojtek1@gmail.com>
2023-01-14 19:56:42 +0100

    Small clean up in ReturnDateNotificationSender

--- src/main/java/com/github/Ksionzka/notification/sender/ReturnDateNotificationSender.java
+++ src/main/java/com/github/Ksionzka/notification/sender/ReturnDateNotificationSender.java
@@ -1,6 +1,5 @@
 package com.github.Ksionzka.notification.sender;
 
-
 import com.github.Ksionzka.notification.ReturnDateNotification;
 import com.github.Ksionzka.persistence.entity.LoanEntity;
 import com.github.Ksionzka.security.email.EmailSender;
@@ -17,76 +16,80 @@ public class ReturnDateNotificationSender implements ApplicationListener<ReturnD
     @Override
     @Async
     public void onApplicationEvent(ReturnDateNotification event) {
+        System.out.println("event");
         LoanEntity loanEntity = event.getLoanEntity();
-        this.emailSender.send(loanEntity.getUser().getEmail(), this.buildEmail(loanEntity));
+        emailSender.send(loanEntity.getUser().getEmail(), getMessage(loanEntity), "Przypomnienie o zwrocie");
     }
 
-    private String buildEmail(LoanEntity loanEntity) {
+    public String getMessage(LoanEntity loan) {
         return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
-            "\n" +
-            "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
-            "\n" +
-            "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
-            "    <tbody><tr>\n" +
-            "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
-            "        \n" +
-            "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
-            "          <tbody><tr>\n" +
-            "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
-            "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
-            "                  <tbody><tr>\n" +
-            "                    <td style=\"padding-left:10px\">\n" +
-            "                  \n" +
-            "                    </td>\n" +
-            "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +
-            "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Zbliża się termin oddania książki</span>\n" +
-            "                    </td>\n" +
-            "                  </tr>\n" +
-            "                </tbody></table>\n" +
-            "              </a>\n" +
-            "            </td>\n" +
-            "          </tr>\n" +
-            "        </tbody></table>\n" +
-            "        \n" +
-            "      </td>\n" +
-            "    </tr>\n" +
-            "  </tbody></table>\n" +
-            "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
-            "    <tbody><tr>\n" +
-            "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +
-            "      <td>\n" +
-            "        \n" +
-            "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
-            "                  <tbody><tr>\n" +
-            "                    <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +
-            "                  </tr>\n" +
-            "                </tbody></table>\n" +
-            "        \n" +
-            "      </td>\n" +
-            "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +
-            "    </tr>\n" +
-            "  </tbody></table>\n" +
-            "\n" +
-            "\n" +
-            "\n" +
-            "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
-            "    <tbody><tr>\n" +
-            "      <td height=\"30\"><br></td>\n" +
-            "    </tr>\n" +
-            "    <tr>\n" +
-            "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
-            "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
-            "        \n" +
-            "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Cześć " + loanEntity.getUser().getFirstName() + ", zbliża się termin oddania książki " + loanEntity.getBook().getRelease().getTitle() + "</p>" +
-            "        \n" +
-            "      </td>\n" +
-            "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
-            "    </tr>\n" +
-            "    <tr>\n" +
-            "      <td height=\"30\"><br></td>\n" +
-            "    </tr>\n" +
-            "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
-            "\n" +
-            "</div></div>";
+                "\n" +
+                "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
+                "\n" +
+                "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
+                "        \n" +
+                "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
+                "          <tbody><tr>\n" +
+                "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
+                "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                  <tbody><tr>\n" +
+                "                    <td style=\"padding-left:10px\">\n" +
+                "                  \n" +
+                "                    </td>\n" +
+                "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +
+                "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Przypomnienie o nadchodzącym terminie zwrotu</span>\n" +
+                "                    </td>\n" +
+                "                  </tr>\n" +
+                "                </tbody></table>\n" +
+                "              </a>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "        </tbody></table>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table>\n" +
+                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +
+                "      <td>\n" +
+                "        \n" +
+                "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                  <tbody><tr>\n" +
+                "                    <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +
+                "                  </tr>\n" +
+                "                </tbody></table>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table>\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
+                "        \n" +
+                "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Cześć " + loan.getUser().getFirstName() + ",</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> Książka: " + loan.getBook().getRelease().getTitle() + " </p>\n <b>Termin zwrotu: </b>" + loan.getReturnDate() +
+                "        \n" +
+                "        \n" +
+                "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> Zbilża się termin oddania wypożyczonej przez Ciebie książki. </p>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
+                "\n" +
+                "</div></div>";
     }
 }
